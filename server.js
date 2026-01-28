053d332d5457952deb622b6a796eb4dc0f2b3cd9
euphoricrisis-code <euphoricrisis@gmail.com>
2026-01-28 07:43:34 +0700

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const NIM_API_KEY = process.env.NIM_API_KEY;
 
 const MODEL_MAPPING = {
   'gpt-3.5-turbo': 'nvidia/llama-3.1-nemotron-ultra-253b-v1',
-  'gpt-4': 'qwen/qwen3-coder-480b-a35b-instruct',
+  'gpt-4': 'google/gemma-3n-e2b-it',
   'gpt-4o': 'deepseek-ai/deepseek-v3.2'
 };
 
